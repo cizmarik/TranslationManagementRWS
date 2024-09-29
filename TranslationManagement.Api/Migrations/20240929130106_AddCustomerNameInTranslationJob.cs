ab0ccbc1c7bd2021a8e1d4e9495544ed92cda6b4
Miro Cizmarik <cizmarikm@gmail.com>
2024-09-29 15:06:13 +0200

    Update TranslationJob with adding TranslatorID

---
+++ TranslationManagement.Api/Migrations/20240929130106_AddCustomerNameInTranslationJob.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TranslationManagement.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerNameInTranslationJob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TranslatorID",
+                table: "TranslationJobs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TranslatorID",
+                table: "TranslationJobs");
+        }
+    }
+}
